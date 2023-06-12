3b0a18c036644924a2745ac68bb716bd8258bb67
VibroMatvey <pooloff2004@gmail.com>
2023-06-12 15:37:23 +0400

    test

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { useProductStore } from '@/stores/products'
+import { useProductStore } from '../store/product.js'
 const route = useRoute()
 const router = useRouter()
 const tg = ref(window.Telegram.WebApp)
